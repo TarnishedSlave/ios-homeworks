5fd7729452e15a50122ef0f0adcdb9ebda6713b0
“Max <morlov321@gmail.com>
2023-03-08 21:50:47 +0800

    1.3 task complete

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Максим Шагдыров on 08.03.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+}
