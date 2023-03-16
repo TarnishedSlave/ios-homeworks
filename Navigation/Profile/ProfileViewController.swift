a7895b864e811cf70e29c18ad7e9d3ae486be9ca
“Max <morlov321@gmail.com>
2023-03-16 21:44:37 +0800

    add new files

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -2,20 +2,24 @@
 //  ProfileViewController.swift
 //  Navigation
 //
-//  Created by Максим Шагдыров on 08.03.2023.
+//  Created by Максим Шагдыров on 16.03.2023.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let profileHeaderView = ProfileHeaderView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.bounds
     }
 }
