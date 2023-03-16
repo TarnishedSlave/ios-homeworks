28634097faf4019bc5cbf5a2ffaf0a13af539a67
“Max <morlov321@gmail.com>
2023-03-16 21:47:26 +0800

    deleted some code

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -1,9 +1,3 @@
-//
-//  ProfileHeaderView.swift
-//  Navigation
-//
-//  Created by Максим Шагдыров on 16.03.2023.
-//
 
 import UIKit
 
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ProfileViewController.swift
-//  Navigation
-//
-//  Created by Максим Шагдыров on 16.03.2023.
-//
 
 import UIKit
 
